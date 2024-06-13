b9f22390d2c55563f1dd3523120bc5da3c9d2002
ardi <aanugerahjanuardi@gmail.com>
2024-06-13 11:06:40 +0700

    change logo

--- resources/views/instructor/master.blade.php
+++ resources/views/instructor/master.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>VMT</title>
-	<link rel="icon" type="image/png" href="{{ asset('/images/icons/logo_1.png') }}"/>
+	<link rel="icon" type="image/png" href="{{ asset('/images/icons/Akmil.png') }}"/>
 	<!-- Google Font: Source Sans Pro -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 	<!-- Font Awesome -->
--- resources/views/instructor/sidebar.blade.php
+++ resources/views/instructor/sidebar.blade.php
@@ -5,7 +5,7 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-        <img src="{{ asset('images/icons/logo_1.png') }}" class="img-circle elevation-2" alt="User Image">
+        <img src="{{ asset('images/icons/Akmil.png') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
         {{-- @if($userID == 0)
